34b5d93748dfbcdb34b50e7c2edc0b71ed5cb0d3
Evgeniy Dorovatovskiy <6276019+gitex@users.noreply.github.com>
2026-07-09 00:42:32 +0500

    chore: start to use tokenizer

--- vector.h
+++ vector.h
@@ -4,41 +4,21 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "types.h"
+#include "core.h"
 #include "arena.h"
 
 
 #define REALLOC(ctx, ptr, size)
 #define FREE(ctx, ptr)
 
-
-#if UINTPTR_MAX == 0xffffffffffffffff
-    #define PLATFORM_64BIT 1
-#elif UINTPTR_MAX == 0xffffffff
-    #define PLATFORM_32BIT 1
-#else
-    #error "Unsupported pointer size"
-#endif  // PLATFORM
-
-
-#if PLATFORM_64BIT
-    typedef i64* IVec;
-#else
-    typedef i32* IVec;
-#endif  // IVec
-
-#if PLATFORM_64BIT
-    typedef u64* UVec;
-#else
-    typedef u32* UVec;
-#endif  // IVec
-
 #define VECTOR_IMPLEMENTATION
 #ifdef VECTOR_IMPLEMENTATION
 
 #define CAP_MIN 4
 #define CAP_FACTOR 2
+#define ALIGNING 32
 
 #define NEXT_CAP(curr_cap) ((curr_cap) * CAP_FACTOR)
 #define PREV_CAP(curr_cap) ((curr_cap) / CAP_FACTOR > CAP_MIN ? (curr_cap) / CAP_FACTOR : CAP_MIN)
@@ -55,6 +35,9 @@ typedef void* VecPtr;
 
 #define header_ptr(vec_ptr) ((VecHeader *) (vec_ptr) - 1)
 #define vector_ptr(header_ptr) ((char *) (header_ptr) + sizeof(VecHeader))
+#define calc_size(cap, elem_size) ((cap) * (elem_size) + sizeof(VecHeader))
+#define curr_size(vec_ptr) calc_size(header_ptr((vec_ptr))->capacity, header_ptr((vec_ptr))->elem_size)
+
 
 #define vec_len(vec_ptr)    header_ptr((vec_ptr))->length
 #define vec_cap(vec_ptr)    header_ptr((vec_ptr))->capacity
@@ -62,36 +45,46 @@ typedef void* VecPtr;
 #define vec_pop(vec)        (header_ptr((vec))->length--, ((vec))[header_ptr((vec))->length])
 #define vec_last(vec)       (vec)[header_ptr(vec)->length - 1]
 
-VecPtr vec_new(Arena *a, size_t capacity, size_t elem_size) {
-    if (capacity < CAP_MIN) {
-        capacity = CAP_MIN;
+VecPtr vec_new(Arena *a, size_t cap, size_t elem_size) {
+    if (cap < CAP_MIN) {
+        cap = CAP_MIN;
+    } else {
+        cap = next_pow2(cap);
     }
 
-    // void *ptr = arena_alloc(a, capacity * elem_size + sizeof(VecHeader), 32);
-    void *ptr = malloc(capacity * elem_size + sizeof(VecHeader));
-    if (!ptr) return NULL;
+    void *h_ptr;
+    if (a != NULL) {
+        h_ptr = arena_alloc(a, calc_size(cap, elem_size), ALIGNING);
+    }
+    if (!h_ptr) return NULL;
 
-    void *vec_ptr = vector_ptr(ptr);
+    void *vec_ptr = vector_ptr(h_ptr);
 
     VecHeader *h = header_ptr(vec_ptr);
     h->length = 0;
-    h->capacity = capacity;
+    h->capacity = cap;
     h->elem_size = elem_size;
+    h->arena = a;
 
     return vec_ptr;
 }
 
-static void *realloc_if_needed(VecPtr vec) {
-    VecHeader *h = header_ptr(vec);
+static VecPtr realloc_if_needed(VecPtr vec) {
+    VecHeader *head = header_ptr(vec);
 
-    if (h->length == h->capacity) {
-        size_t next_cap = NEXT_CAP(h->capacity);
-        size_t new_size = next_cap * h->elem_size + sizeof(VecHeader);
+    if (head->length == head->capacity) {
+        size_t next_cap = NEXT_CAP(head->capacity);
+        size_t new_size = calc_size(next_cap, head->elem_size);
 
-        void *tmp = realloc(h, new_size);
-        if (!tmp) return NULL;
+        void *new_header_ptr = NULL;
+        if (head->arena != NULL) {
+            new_header_ptr = arena_alloc(head->arena, new_size, ALIGNING);
+        }
+        if (new_header_ptr == NULL) return NULL;
 
-        vec = vector_ptr(tmp);
+        memcpy(new_header_ptr, head, calc_size(head->capacity, head->elem_size));
+
+        vec = vector_ptr(new_header_ptr);
         header_ptr(vec)->capacity = next_cap;
     }
     return vec;
